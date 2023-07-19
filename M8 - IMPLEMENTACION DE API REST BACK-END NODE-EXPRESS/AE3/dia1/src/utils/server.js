8807b25ab990b867aa8bb46fc7635f4b6ce395ba
NicteAC <carola.aliaga@gmail.com>
2023-07-19 10:33:37 -0400

    update

---
+++ M8 - IMPLEMENTACION DE API REST BACK-END NODE-EXPRESS/AE3/dia1/src/utils/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import exphbs from "express-handlebars";
+import fileUpload from "express-fileupload";
+import fs from "fs";
+
+const app = express();
+app.listen(3000);
+
+app.engine("hbs", exphbs.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(
+  fileUpload({
+    limits: { fileSize: 5000000 },
+    abortOnLimit: true,
+    responseOnLimit: "El peso del archivo excede el limite permitido",
+  })
+);
+
+app.get("/", (req, res) => res.render("home"));
+
+app.post("/subirfoto", (req, res) => {
+  const { nombre, apellido } = req.body;
+  const { foto } = req.files;
+  const fileName = `${nombre}_${apellido}_${foto.name}`;
+  foto.mv(`${process.cwd()}/src/utils/uploads/${fileName}`, (err) => {
+    if (err)
+      return res.status(500).json({
+        code: 500,
+        message: "Ha ocurrido un error en el servidor",
+        error: err,
+      });
+    res.send("Tu imagen fue cargada con exito");
+  });
+});
+
+app.post("/eliminarfoto", (req, res) => {
+  try {
+    const { filedelete } = req.body;
+    fs.unlinkSync(`${process.cwd()}/src/utils/uploads/${filedelete}`);
+    res.send("imagen eliminada con éxito");
+  } catch (error) {
+    console.error(error);
+    res.send("Error al eliminar la imagen");
+  }
+});
+
+app.all("*", (req, res) =>
+  res.status(404).json({ code: 404, message: "Página no encontrada" })
+);
+
+export { app };
